e821532ed0286e6eb929cdff50397946c1f83b29
Marius 't Hart <thartbm@gmail.com>
2025-04-02 09:30:55 -0400

    generic function to read participant data

--- R/utilities.R
+++ R/utilities.R
@@ -1,5 +1,22 @@
 
 
+`%notin%` <- Negate(`%in%`)
+
+# loads the data for 1 participant:
+
+loadParticipantTaskData <- function(ID, task) {
+  
+  # both halves of the data should be in one file now:
+  data <- read.table(file = sprintf('data/sub-%s/ses-%s/beh/sub-%s_%s.tsv',ID,task,ID,task), header = TRUE, sep = "\t", skip = 0)
+  
+  # remove lines we don't need:
+  data <- data[which(data$abort == FALSE),]
+  
+  # that's all we want for now:
+  return(data)
+  
+}
+
 
 setupFigureFile <- function(target='inline',width=8,height=6,dpi=300,filename) {
   
