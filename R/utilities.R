a81f8b249502ae4de8e6331dc1c1aadd5b224c3f
Marius 't Hart <thartbm@gmail.com>
2025-04-24 13:45:34 -0400

    luminance calculation for binocular control task

--- R/utilities.R
+++ R/utilities.R
@@ -50,3 +50,47 @@ setupFigureFile <- function(target='inline',width=8,height=6,dpi=300,filename) {
   }
 }
 
+# luminance of stimuli ----
+
+printStimulusLuminance <- function() {
+  
+  # CLUT was linearized by PsychoPy,
+  # so values are in linear space.
+  
+  # [ [  0., 135.44739,  2.4203537, np.nan, np.nan, np.nan  ],
+  #   [  0.,  27.722954, 2.4203537, np.nan, np.nan, np.nan  ],
+  #   [  0.,  97.999275, 2.4203537, np.nan, np.nan, np.nan  ],
+  #   [  0.,   9.235623, 2.4203537, np.nan, np.nan, np.nan  ]  ],
+  
+  maxLums <- c(27.722954, 97.999275, 9.235623)
+  
+  background <-	(c(0.50000000,0.50000000,-1.00000000)  + 1) / 2
+  red        <-	(c(0.50000000,-1.00000000,-1.00000000) + 1) / 2
+  green      <- (c(-1.00000000,0.50000000,-1.00000000) + 1) / 2
+  
+  # luminances
+  cat(sprintf('background: %0.3f\n',sum(background * maxLums)))
+  cat(sprintf('red:        %0.3f\n',sum(red        * maxLums)))
+  cat(sprintf('green:      %0.3f\n',sum(green      * maxLums)))
+  
+  Rmax = 27.722954
+  Gmax = 97.999275
+  Rw = Rmax / (Rmax+Gmax)
+  Lw = Gmax / (Rmax+Gmax)
+  col_binoc <- c()
+  for (idx in c(1:3)) {
+    col_binoc[idx] <- (Lw*red[idx]) + (Rw*green[idx])
+  }
+  
+  cat(sprintf('binocular:  %0.3f (lum weighted)\n',sum(col_binoc * maxLums)))
+  col_binoc <- c()
+  for (idx in c(1:3)) {
+    col_binoc[idx] <- (.5*red[idx]) + (.5*green[idx])
+  }
+  
+  cat(sprintf('binocular:  %0.3f (50-50)\n',sum(col_binoc * maxLums)))
+  cat(sprintf('red&green:  %0.3f\n',mean(c(sum(red        * maxLums),sum(green      * maxLums)))))
+  
+  
+  
+}
