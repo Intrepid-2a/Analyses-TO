af845bd055723ee0308a78ac9ab51411dd9024f8
Marius 't Hart <thartbm@gmail.com>
2025-04-24 13:46:39 -0400

    test of which stimulus was presented first in distance task

--- R/distance3.R
+++ R/distance3.R
@@ -821,7 +821,7 @@ getOrigDistANOVAdata <- function() {
   locations <- unique(data$Location)
   origs <- unique(data$Orig)
   
-  print(origs)
+  # print(origs)
   
   # create a data frame with all combinations of participants, eyes, and locations
   df <- expand.grid(participant = participants, Eye = eyes, Location = locations, Orig = origs)
@@ -882,6 +882,270 @@ doOrigDistanceStats <- function() {
     dv = "slope",
     data = data,
     within = c("Eye", "Location", "Orig"),
+  )
+  
+  cat("\n==== Distance Slope ANOVA:\n\n")
+  
+  print(slope_aov)
+  
+  
+}
+
+
+# split by first stimulus ----
+
+processFirstDistanceData <- function() {
+  
+  allData <- loadDistanceData()
+  
+  info <- data.frame( eye   = c('ipsi',  'ipsi',  'cont',  'cont',  'ipsi', 'ipsi', 'cont', 'cont'),
+                      area  = c('bsa',   'out',   'bsa',   'out',   'bsa',  'out',  'bsa',  'out'),
+                      first = c('target','target','target','target','foil', 'foil', 'foil', 'foil'))
+  
+  allConditionData <- list()
+  
+  for (cond_no in c(1:nrow(info))) {
+    
+    # select rows that correspond to one of the 4 conditions:
+    eye   <- info$eye[  cond_no]
+    area  <- info$area[ cond_no]
+    first <- info$first[cond_no]
+    
+    condition_data <- NA
+    
+    for (participant in names(allData)) {
+      
+      df <- allData[[participant]]
+      
+      df$Which_first[which(df$Which_first == 'Targ')] <- 'target'
+      df$Which_first[which(df$Which_first == 'Foil')] <- 'foil'
+      
+      if (eye == 'ipsi') {
+        e_idx <- which(df$HemiField == df$Eye)
+      } else {
+        e_idx <- which(df$HemiField != df$Eye)
+      }
+      
+      if (area == 'bsa') {
+        a_idx <- which(df$Targ_loc    %in% c('left-mid','righ-mid'))
+      } else {
+        a_idx <- which(df$Targ_loc %notin% c('left-mid','righ-mid'))
+      }
+      
+      f_idx <- which(df$Which_first == first)
+      
+      idx <- intersect(intersect(e_idx, a_idx),f_idx)
+      
+      # print(length(idx))
+      
+      cdf <- df[idx,]
+      cdf$count <- 1
+      
+      agcdf  <- aggregate(Targ_chosen ~ Difference, data=cdf, FUN=mean)
+      agccdf <- aggregate(count       ~ Difference, data=cdf, FUN=sum)
+      
+      agcdf$participant <- participant
+      agcdf$count       <- agccdf$count
+      
+      if (is.data.frame(condition_data)) {
+        condition_data <- rbind(condition_data, agcdf)
+      } else {
+        condition_data <- agcdf
+      }
+      
+    }
+    
+    allConditionData[[sprintf('%s_%s_%s', eye, area, first)]] <- condition_data
+    
+  }
+  
+  return(allConditionData)
+  
+}
+
+plotFirstDistancePsychometricCurves <- function(target='inline') {
+  
+  setupFigureFile( target = target, 
+                   width = 10, 
+                   height = 5, 
+                   dpi = 300, 
+                   filename = sprintf('doc/fig/%s/distance_psychometric_origineye.%s', target, target))
+  
+  
+  allConditionData <- processFirstDistanceData()
+  
+  N <- length(unique(allConditionData[[1]]$participant))
+  
+  layout(mat=matrix(data=c(1,2), nrow=1, ncol=2, byrow=TRUE))
+  
+  for (first in c('target','foil')) {
+    
+    plot(-1000,-1000,
+         xlim=c(-3.5, 3.5), ylim=c(0,1),
+         main=sprintf('distance perception (N=%d, %s first)',N,first), xlab='difference [dva]', ylab='proportion target chosen',
+         ax=F, bty='n')
+    
+    lines(x=c(-3.5,3.5),y=c(0.5,0.5),col='gray',lty=3)
+    lines(x=c(0,0),y=c(0,1),col='gray',lty=3)
+    
+    info <- data.frame( eye  = c('ipsi','ipsi','cont','cont'),
+                        area = c('bsa', 'out', 'bsa', 'out'),
+                        col  = c('red', 'orange', 'blue', 'turquoise'))
+    
+    
+    for (cond_no in c(1:nrow(info))) {
+      
+      scol <- info$col[cond_no]
+      tcol <- Reach::colorAlpha(scol, alpha=34)
+      
+      cdf <- allConditionData[[sprintf('%s_%s_%s', info$eye[cond_no], info$area[cond_no], first)]]
+      
+      acdf <- aggregate(Targ_chosen ~ Difference, data=cdf, FUN=mean)
+      lines(acdf, col=Reach::colorAlpha(scol, alpha=100))
+      points(acdf, col=Reach::colorAlpha(scol, alpha=100))
+      
+      mprob <- fit.mprobit(y=acdf$Targ_chosen, x=acdf$Difference, 
+                           start = c( 0, 1,   0,   0  ),
+                           lower = c(-3, 0.2, 0,   0  ),
+                           upper = c( 3, 3,   0.3, 0.3)
+      )
+      X <- seq(-3.5,3.5,0.1)
+      lines(x=X, y=mprobit(p=mprob$par, x=X), lwd = 1, col = scol)
+      
+    }
+    
+    legend(x=0, y=0.5,
+           legend=c('ipsi, at', 'ipsi, away', 'contra, at', 'contra, away'),
+           lty=1, col=info$col,
+           bty='n')
+    
+    axis(side=1, at=c(-3.5,-2,-1,0,1,2,3.5))
+    axis(side=2, at=c(0,0.5,1))
+    
+  }
+  
+  
+  if (target %in% c('pdf','svg','png','tiff')) {
+    dev.off()
+  }
+  
+}
+
+
+## statistics -----
+
+getAllFirstDistData <- function() {
+  
+  allConditionData <- processFirstDistanceData()
+  
+  info <- data.frame( eye   = c('ipsi',  'ipsi',  'cont',  'cont',  'ipsi', 'ipsi', 'cont', 'cont'),
+                      area  = c('bsa',   'out',   'bsa',   'out',   'bsa',  'out',  'bsa',  'out'),
+                      first = c('target','target','target','target','foil', 'foil', 'foil', 'foil'))
+  
+  allData <- NA
+  
+  for (condition in names(allConditionData)) {
+    
+    cond_df <- allConditionData[[condition]]
+    
+    
+    if (condition %in% c("ipsi_bsa_target", "ipsi_out_target", "ipsi_bsa_foil", "ipsi_out_foil")) {
+      cond_df$Eye <- 'ipsilateral'
+    } else {
+      cond_df$Eye <- 'contralateral'
+    }
+    if (condition %in% c("ipsi_bsa_target", "cont_bsa_target", "ipsi_bsa_foil", "cont_bsa_foil")) {
+      cond_df$Location <- 'blindspot'
+    } else {
+      cond_df$Location <- 'away'
+    }
+    if (condition %in% c("ipsi_bsa_foil", "cont_bsa_foil", "ipsi_out_foil", "cont_out_foil")) {
+      cond_df$First <- 'foil'
+    } else {
+      cond_df$First <- 'target'
+    }
+    if (is.data.frame(allData)) {
+      allData <- rbind(allData, cond_df)
+    } else {
+      allData <- cond_df
+    }
+    
+  }
+  
+  return(allData)
+  
+}
+
+getFirstDistANOVAdata <- function() {
+  
+  data <- getAllFirstDistData()
+  
+  participants <- unique(data$participant)
+  eyes <- unique(data$Eye)
+  locations <- unique(data$Location)
+  firsts <- unique(data$First)
+  
+  # print(origs)
+  
+  # create a data frame with all combinations of participants, eyes, and locations
+  df <- expand.grid(participant = participants, Eye = eyes, Location = locations, First = firsts)
+  
+  df$mean  <- NA
+  df$sd    <- NA
+  df$margL <- NA
+  df$margU <- NA
+  df$PSE   <- NA
+  df$slope <- NA
+  
+  for (i in 1:nrow(df)) {
+    participant <- df$participant[i]
+    eye        <- df$Eye[i]
+    location   <- df$Location[i]
+    first      <- df$First[i]
+    
+    # filter the data for the current combination
+    subdf <- data[data$participant == participant & data$Eye == eye & data$Location == location & data$First == first, ]
+    
+    mod <- fit.mprobit( y=subdf$Targ_chosen, x =subdf$Difference,
+                        start = c(-0.5,   1, 0,   0  ),
+                        lower = c(-3,   0.3, 0,   0  ),
+                        upper = c( 3,     3, 0.3, 0.3) )
+    
+    df$mean[i] <- mod$par[1]
+    df$sd[i]   <- mod$par[2]
+    df$margL[i] <- mod$par[3]
+    df$margU[i] <- mod$par[4]
+    
+    descr <- descr.mprobit(p=mod$par)
+    df$PSE[i]   <- descr$PSE
+    df$slope[i] <- descr$slope
+    
+  }
+  
+  return(df)
+}
+
+doFirstDistanceStats <- function() {
+  
+  data <- getFirstDistANOVAdata()
+  
+  # fit the model
+  bias_aov <- afex::aov_ez(
+    id = "participant",
+    dv = "PSE",
+    data = data,
+    within = c("Eye", "Location", "First"),
+  )
+  
+  cat("==== Distance Bias ANOVA:\n\n")
+  
+  print(bias_aov)
+  
+  slope_aov <- afex::aov_ez(
+    id = "participant",
+    dv = "slope",
+    data = data,
+    within = c("Eye", "Location", "First"),
   )
   
   cat("\n==== Distance Slope ANOVA:\n\n")
